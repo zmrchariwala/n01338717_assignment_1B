6f42be16ec43d91cf6034a52ded54f92de335510
Zameer Chariwala <zmrchariwala@gmail.com>
2019-09-24 22:31:48 -0400

    Final

--- bus_tour_review/bus_tour_review.aspx.cs
+++ bus_tour_review/bus_tour_review.aspx.cs
@@ -21,93 +21,93 @@ namespace bus_tour_review
                     string review_message = passenger_review_message.Text.ToString();
                     string passenger_emailaddress = passenger_email.Text.ToString();
                     string tour_experience = passenger_experience.Text.ToString();
-                    int tour_rate = Convert.ToInt32(tour_rating.Text.ToString());
+                    int tour_star = Convert.ToInt32(tour_rating.Text.ToString());
                     string display_overall_rating;
                     display_overall_rating = "Review Title: "+review_title+"<br/>";
                     display_overall_rating += "Review Message: " + review_message + "<br/>";
-                    display_overall_rating +="E-mail address"+passenger_emailaddress+"<br/>";
-                    display_overall_rating +="Tour experience: "+passenger_emailaddress+"<br/>";
-                    display_overall_rating+="Rating: "+tour_rate+"<br/>";
+                    display_overall_rating +="E-mail address: "+passenger_emailaddress+"<br/>";
+                    display_overall_rating +="Tour experience: "+tour_experience+"<br/>";
+                    display_overall_rating+="Rating: "+tour_star+"<br/>";
 
-                    if(tour_experience=="Very good"&&tour_rate==5)
+                    if(tour_experience=="Very good"&&tour_star==5)
                     {
-                        display_overall_rating += "Overall rating: 5";
+                        display_overall_rating += "Overall Star: 5";
                     }
-                    else if (tour_experience == "Good" && tour_rate == 5)
+                    else if (tour_experience == "Good" && tour_star == 5)
                     {
-                        display_overall_rating += "Overall rating: 4";
+                        display_overall_rating += "Overall Star: 4";
                     }
-                    else if (tour_experience == "Fair" && tour_rate == 5)
+                    else if (tour_experience == "Fair" && tour_star == 5)
                     {
-                        display_overall_rating += "Overall rating: 3";
+                        display_overall_rating += "Overall Star: 3";
                     }
-                    else if (tour_experience == "Poor" && tour_rate == 5)
+                    else if (tour_experience == "Poor" && tour_star == 5)
                     {
-                        display_overall_rating += "Overall rating: 2";
+                        display_overall_rating += "Overall Star: 2";
                     }
-                    else if (tour_experience == "Very good" && tour_rate == 4)
+                    else if (tour_experience == "Very good" && tour_star == 4)
                     {
-                        display_overall_rating += "Overall rating: 4";
+                        display_overall_rating += "Overall Star: 4";
                     }
-                    else if (tour_experience == "Good" && tour_rate == 4)
+                    else if (tour_experience == "Good" && tour_star == 4)
                     {
-                        display_overall_rating += "Overall rating: 3";
+                        display_overall_rating += "Overall Star: 3";
                     }
-                    else if (tour_experience == "Fair" && tour_rate == 4)
+                    else if (tour_experience == "Fair" && tour_star == 4)
                     {
-                        display_overall_rating += "Overall rating: 2";
+                        display_overall_rating += "Overall Star: 2";
                     }
-                    else if (tour_experience == "Poor" && tour_rate == 4)
+                    else if (tour_experience == "Poor" && tour_star == 4)
                     {
-                        display_overall_rating += "Overall rating: 2";
+                        display_overall_rating += "Overall Star: 2";
                     }
-                    else if (tour_experience == "Very good" && tour_rate == 3)
+                    else if (tour_experience == "Very good" && tour_star == 3)
                     {
-                        display_overall_rating += "Overall rating: 4";
+                        display_overall_rating += "Overall Star: 4";
                     }
-                    else if (tour_experience == "Good" && tour_rate == 3)
+                    else if (tour_experience == "Good" && tour_star == 3)
                     {
-                        display_overall_rating += "Overall rating: 3";
+                        display_overall_rating += "Overall Star: 3";
                     }
-                    else if (tour_experience == "Fair" && tour_rate == 3)
+                    else if (tour_experience == "Fair" && tour_star == 3)
                     {
-                        display_overall_rating += "Overall rating: 2";
+                        display_overall_rating += "Overall Star: 2";
                     }
-                    else if (tour_experience == "Poor" && tour_rate == 3)
+                    else if (tour_experience == "Poor" && tour_star == 3)
                     {
-                        display_overall_rating += "Overall rating: 1";
+                        display_overall_rating += "Overall Star: 1";
                     }
-                    else if (tour_experience == "Very good" && tour_rate == 2)
+                    else if (tour_experience == "Very good" && tour_star == 2)
                     {
-                        display_overall_rating += "Overall rating: 4";
+                        display_overall_rating += "Overall Star: 4";
                     }
-                    else if (tour_experience == "Good" && tour_rate == 2)
+                    else if (tour_experience == "Good" && tour_star == 2)
                     {
-                        display_overall_rating += "Overall rating: 3";
+                        display_overall_rating += "Overall Star: 3";
                     }
-                    else if (tour_experience == "Fair" && tour_rate == 2)
+                    else if (tour_experience == "Fair" && tour_star == 2)
                     {
-                        display_overall_rating += "Overall rating: 2";
+                        display_overall_rating += "Overall Star: 2";
                     }
-                    else if (tour_experience == "Poor" && tour_rate == 2)
+                    else if (tour_experience == "Poor" && tour_star == 2)
                     {
-                        display_overall_rating += "Overall rating: 1";
+                        display_overall_rating += "Overall Star: 1";
                     }
-                    else if (tour_experience == "Very good" && tour_rate == 1)
+                    else if (tour_experience == "Very good" && tour_star == 1)
                     {
-                        display_overall_rating += "Overall rating: 2";
+                        display_overall_rating += "Overall Star: 2";
                     }
-                    else if (tour_experience == "Good" && tour_rate == 1)
+                    else if (tour_experience == "Good" && tour_star == 1)
                     {
-                        display_overall_rating += "Overall rating: 2";
+                        display_overall_rating += "Overall Star: 2";
                     }
-                    else if (tour_experience == "Fair" && tour_rate == 1)
+                    else if (tour_experience == "Fair" && tour_star == 1)
                     {
-                        display_overall_rating += "Overall rating: 2";
+                        display_overall_rating += "Overall Star: 2";
                     }
-                    else if (tour_experience == "Poor" && tour_rate == 1)
+                    else if (tour_experience == "Poor" && tour_star == 1)
                     {
-                        display_overall_rating += "Overall rating: 1";
+                        display_overall_rating += "Overall Star: 1";
                     }
                     else
                     {
